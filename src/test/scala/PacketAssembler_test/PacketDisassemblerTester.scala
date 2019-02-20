6a52795f2a29a312d5b3dd490615c0ab60143a4b
jerryduan07 <jerry_duan@berkeley.edu>
2019-02-19 23:38:43 -0800

    add function in pda unit test

--- src/test/scala/PacketAssembler_test/PacketDisassemblerTester.scala
+++ src/test/scala/PacketAssembler_test/PacketDisassemblerTester.scala
@@ -7,19 +7,49 @@ import chisel3.iotesters.{PeekPokeTester, Driver, ChiselFlatSpec}
 
 class PacketDisAssemblerTest(c: PacketDisAssembler) extends PeekPokeTester(c) {
 
-  val data_preamble = "h55".U
-  val data_AA = "h6B7D9171".U
-  val data_AA_rec = "h8E89BED6".U
-  val data_pduH = "hF1BB".U
-  val data_pduH_rec = "h0F02".U
-  val data_pduAA = "h8984F0AB260D".U
-  val data_pduAA_rec = "h0002723280C6".U
-  val data_pduData1 = "hEE0C28B279".U
-  val data_pduData1_rec = "h0805050102".U
-  val data_pduData2 = "hA283CBA0".U
-  val data_pduData2_rec = "h43303932".U
-  val data_crc = "h748AAD".U
-  val data_crc_rec = "h65FAC7".U
+  val data_preamble = BigInt("55", 16)
+  val data_AA = BigInt("6B7D9171", 16)
+  val data_AA_rec = BigInt("8E89BED6", 16)
+  val data_pduH = BigInt("F1BB", 16)
+  val data_pduH_rec = BigInt("0F02", 16)
+  val data_pduAA = BigInt("8984F0AB260D", 16)
+  val data_pduAA_rec = BigInt("0002723280C6", 16)
+  val data_pduData1 = BigInt("EE0C28B279", 16)
+  val data_pduData1_rec = BigInt("0805050102", 16)
+  val data_pduData2 = BigInt("A283CBA0", 16)
+  val data_pduData2_rec = BigInt("43303932", 16)
+  val data_crc = BigInt("748AAD", 16)
+  val data_crc_rec = BigInt("65FAC7", 16)
+  val pdu_length_1 = 40
+  val pdu_length_2 = 32 
+  
+
+  def sendBits(
+      data_in: BigInt,
+      expected_out: BigInt,
+      length: Int
+  ) = {
+
+  for (j <- 0 to length - 1) {
+    poke(c.io.in.bits.data, data_in.U(length-1-j))
+    poke(c.io.in.valid, true.B)
+    poke(c.io.out.ready, true.B)
+    if(j % 8 == 7) {
+        step(1)
+        poke(c.io.in.valid, false.B)
+        expect(c.io.out.bits.data, expected_out.U(j, j-7))
+        expect(c.io.out.valid, true.B)
+        println(s"j="+j+s"\n${peek(c.io.out.bits.data)}\t${peek(expected_out.U(j, j-7))}")
+    }else{
+      expect(c.io.out.valid, false.B)
+    }
+    step(1)
+    poke(c.io.out.ready, false.B)
+  }
+  poke(c.io.in.valid, false.B)
+  step(10)
+  }
+
 
 
   //reset
@@ -47,7 +77,7 @@ class PacketDisAssemblerTest(c: PacketDisAssembler) extends PeekPokeTester(c) {
   //PREAMBLE
   for (j <- 0 to 7) {
     poke(c.io.out.ready, true.B)
-    poke(c.io.in.bits.data, data_preamble(7-j)) //note: U to B
+    poke(c.io.in.bits.data, data_preamble.U(7-j)) //note: U to B
     expect(c.io.out.valid, false.B)
     step(1)
     poke(c.io.out.ready, false.B)
@@ -56,106 +86,22 @@ class PacketDisAssemblerTest(c: PacketDisAssembler) extends PeekPokeTester(c) {
   step(10)
 
   //AA
-  for (j <- 0 to 31) {    
-    poke(c.io.in.bits.data, data_AA(31-j))
-    poke(c.io.in.valid, true.B)
-    poke(c.io.out.ready, true.B)
-    if(j % 8 == 7) {
-        step(1)
-        poke(c.io.in.valid, false.B)
-        expect(c.io.out.bits.data, data_AA_rec(j, j-7))
-        println(s"j="+j+s"\n${peek(c.io.out.bits.data)}\t${peek(data_AA_rec(j, j-7))}")  
-    }
-    step(1)
-    poke(c.io.out.ready, false.B)
-  }
-  poke(c.io.in.valid, false.B)
-  step(10)
+  sendBits(data_AA, data_AA_rec, 32)
 
   //PDU_HEADER
-  for (j <- 0 to 15) {
-    poke(c.io.in.bits.data, data_pduH(15-j))
-    poke(c.io.in.valid, true.B)
-    poke(c.io.out.ready, true.B)
-    if(j % 8 == 7) {
-        step(1)
-        poke(c.io.in.valid, false.B)
-        expect(c.io.out.bits.data, data_pduH_rec(j, j-7))
-        println(s"j="+j+s"\n${peek(c.io.out.bits.data)}\t${peek(data_pduH_rec(j, j-7))}")
-    }
-    step(1)
-    poke(c.io.out.ready, false.B)
-  }
-  poke(c.io.in.valid, false.B)
-  step(10)
+  sendBits(data_pduH, data_pduH_rec, 16)
 
   //PDU_AA
-  for (j <- 0 to 47) {
-    poke(c.io.in.bits.data, data_pduAA(47-j))
-    poke(c.io.in.valid, true.B)
-    poke(c.io.out.ready, true.B)
-    if(j % 8 == 7) {
-        step(1)
-        poke(c.io.in.valid, false.B)
-        expect(c.io.out.bits.data, data_pduAA_rec(j, j-7))
-        println(s"j="+j+s"\n${peek(c.io.out.bits.data)}\t${peek(data_pduAA_rec(j, j-7))}")
-    }
-    step(1)
-    poke(c.io.out.ready, false.B)
-  }
-  poke(c.io.in.valid, false.B)
-  step(10)
+  sendBits(data_pduAA, data_pduAA_rec, 48)
 
-//PDU Data 1
-  for (j <- 0 to 39) {
-    poke(c.io.in.bits.data, data_pduData1(39-j))
-    poke(c.io.in.valid, true.B)
-    poke(c.io.out.ready, true.B)
-    if(j % 8 == 7) {
-        step(1)
-        poke(c.io.in.valid, false.B)
-        expect(c.io.out.bits.data, data_pduData1_rec(j, j-7))
-        println(s"j="+j+s"\n${peek(c.io.out.bits.data)}\t${peek(data_pduData1_rec(j, j-7))}")
-    }
-    step(1)
-    poke(c.io.out.ready, false.B)
-  }
-  poke(c.io.in.valid, false.B)
-  step(10)
+  //PDU Data 1
+  sendBits(data_pduData1, data_pduData1_rec, 40)
 
-//PDU Data 2
-  for (j <- 0 to 31) {
-    poke(c.io.in.bits.data, data_pduData2(31-j))
-    poke(c.io.in.valid, true.B)
-    poke(c.io.out.ready, true.B)
-    if(j % 8 == 7) {
-        step(1)
-        poke(c.io.in.valid, false.B)
-        expect(c.io.out.bits.data, data_pduData2_rec(j, j-7))
-        println(s"j="+j+s"\n${peek(c.io.out.bits.data)}\t${peek(data_pduData2_rec(j, j-7))}")
-    }    
-    step(1)
-    poke(c.io.out.ready, false.B)
-  }
-  poke(c.io.in.valid, false.B)
-  step(10)
+  //PDU Data 2
+  sendBits(data_pduData2, data_pduData2_rec, 32)
 
   //CRC
-  for (j <- 0 to 23) {
-    poke(c.io.in.bits.data, data_crc(23-j))
-    poke(c.io.in.valid, true.B)
-    poke(c.io.out.ready, true.B)
-    if(j % 8 == 7) {
-        step(1)
-        poke(c.io.in.valid, false.B)
-        expect(c.io.out.bits.data, data_crc_rec(j, j-7))
-        println(s"j="+j+s"\n${peek(c.io.out.bits.data)}\t${peek(data_crc_rec(j, j-7))}")
-    }    
-    step(1)
-    poke(c.io.out.ready, false.B)
-  }
-  poke(c.io.in.valid, false.B)
-  step(10)
+  sendBits(data_crc, data_crc_rec, 24)
 
   expect(c.io.out.bits.done, true.B)
   //todo: add FIFO
